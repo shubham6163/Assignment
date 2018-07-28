a7bcfa065ddfa6bcd5674bca15e9e87a34b9921a
Shubham Goswami <shubhamgoswami@myworkforce.org>
2018-07-28 16:26:15 +0530

    Initial Commit

---
+++ AssignmentInfosys/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AssignmentInfosys
+//
+//  Created by Shubham Goswami on 28/07/18.
+//  Copyright © 2018 Shubham Goswami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
